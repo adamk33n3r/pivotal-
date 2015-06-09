4c94ae9c0f472ff6d94f8b2a66adc6e6eb310988
Adam Keenan <adam.keenan@myfarms.com>
2015-06-09 17:32:35 -0400

    Added pay period buttons. Better init message.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,29 +1,36 @@
 
 var bg = "#256188";
+var nbsp = "\u00A0";
+var arrow = "\u25B6";
+
 var consoleArgs = [
-    "%c Pivotal++ %c▶%c initializing... ",
-    "background:"+bg+";color:white;font-size:20px",
-    "font-size:25px;color:"+bg+";border:1px "+bg+" solid;padding-bottom:2px;border-right:none",
-    "font-size:20px;color:"+bg+";border:1px "+bg+" solid;padding:4px 0 3px 0;border-left:none;"
+    "%c Pivotal++"+nbsp+"%c %c"+nbsp+"initializing..."+nbsp,
+    "font-size:10px;color:white;border:1px "+bg+" solid;border-right:none;background:"+bg+";line-height:20px",
+    "font-size:12px;color:"+bg+";border:1px "+bg+" solid;padding-bottom:1px;",
+    "font-size:10px;color:"+bg+";border:1px "+bg+" solid;border-left:none;"
 ];
 
-
-for (var i = 2; i < consoleArgs.length; i++) {
-    //consoleArgs[i] = "line-height:30px;" + consoleArgs[i];
-}
+/* No "bandaid"
+consoleArgs = [
+    "%c Pivotal++"+nbsp+"%c"+nbsp+"initializing..."+nbsp,
+    "font-size:10px;color:white;border:1px "+bg+" solid;border-right:none;background:"+bg+";",
+    "font-size:10px;color:"+bg+";border:1px "+bg+" solid;border-left:none;"
+];
+*/
 
 console.log.apply(console, consoleArgs);
 
 var buttonCSS = " -moz-border-radius: 3px;\
                   -webkit-border-radius: 3px;\
                   border-radius: 3px;\
-                  display: inline;\
+                  display: inline-block;\
                   background-color: #3472a4;\
                   color: #FFF;\
                   border: none;\
                   font-size: 11px;\
                   font-weight: bold;\
-                  padding: 7px 14px;"
+                  padding: 0 14px;\
+                  line-height: 25px;"
 
 
 
@@ -63,12 +70,26 @@ function time_shifts_new_edit() {
 
     $cql.html($cql.html().replace("|", ""));
     jQuery("#yesterday, #today").attr("style", buttonCSS);
-    jQuery("#yesterday").css("border-top-right-radius", 0);
-    jQuery("#yesterday").css("border-bottom-right-radius", 0);
-    jQuery("#today").css("border-top-left-radius", 0);
-    jQuery("#today").css("border-bottom-left-radius", 0);
+    var $yesterday = jQuery("#yesterday");
+    $yesterday.css("border-top-right-radius", 0);
+    $yesterday.css("border-bottom-right-radius", 0);
+    var $today = jQuery("#today");
+    $today.css("border-top-left-radius", 0);
+    $today.css("border-bottom-left-radius", 0);
 }
 
 function time_shifts() {
     jQuery("#new_shift_button").attr("style", buttonCSS);
+    var $spp = jQuery("#set_pay_period");
+    $spp.css("font-size", 0)
+        .css("margin-left", "5px");
+    $spp.html($spp.html().replace("|", ""));
+
+    var $yesterday = $spp.children("a").first();
+    var $today = $spp.children("a").last();
+    jQuery($yesterday.add($today)).attr("style", buttonCSS);
+    $yesterday.css("border-top-right-radius", 0);
+    $yesterday.css("border-bottom-right-radius", 0);
+    $today.css("border-top-left-radius", 0);
+    $today.css("border-bottom-left-radius", 0);
 }
